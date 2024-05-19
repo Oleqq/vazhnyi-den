998a982176d8d9f4f40feb0e82593fb9699fd71a
Oleg Zagneev <tovilka228@gmail.com>
2024-05-20 01:45:17 +0300

    Работа с главной страницей сайта
    Разработка адаптива

--- js/front.js
+++ js/front.js
@@ -1,3 +1,7 @@
+
+
+
+
 const swiper = new Swiper('.portfolio__slider', {
 	direction: 'horizontal',
 	loop: true,
@@ -100,3 +104,111 @@ const swiper = new Swiper('.portfolio__slider', {
         });
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	const menu = document.querySelector('.nav'),
+    burger = document.querySelector('.burger'),
+    overlay = document.querySelector('.overlay');
+
+const lockScroll = () => {
+    document.body.classList.add('lock');
+    overlay.classList.add('open'); // Добавляем класс open к overlay
+}
+
+const unlockScroll = () => {
+    document.body.classList.remove('lock');
+    overlay.classList.remove('open'); // Удаляем класс open с overlay
+}
+
+const initialMenu = () => {
+    document.querySelector('.nav__list--dropdown').classList.remove('transformation');
+    document.querySelector('.nav').querySelector('.nav__list').classList.remove('transformation');
+    scrollTop();
+}
+
+const scrollTop = () => {
+    menu.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+}
+
+const closeMenu = () => {
+    menu.classList.remove('open');
+    unlockScroll();
+}
+
+burger.addEventListener('click', (e) => {
+    e.stopPropagation(); // Предотвращение всплытия события
+    if (menu.classList.contains('open')) {
+        closeMenu();
+    } else {
+        menu.classList.add('open');
+        lockScroll();
+        initialMenu();
+    }
+});
+
+overlay.addEventListener('click', (e) => {
+    closeMenu();
+});
+
+menu.addEventListener('click', (e) => {
+    e.stopPropagation(); // Предотвращение всплытия события
+    if (e.target.classList.contains('nav__link--drop')) {
+        e.preventDefault();
+        e.target.closest('.nav__list').classList.add('transformation');
+        e.target.closest('.nav__item').querySelector('.nav__list--dropdown').classList.add('transformation');
+        scrollTop();
+    }
+
+    if (e.target.classList.contains('mobile-back__link')) {
+        e.preventDefault();
+        e.target.closest('.nav__list--dropdown').classList.remove('transformation');
+        e.target.closest('.nav').querySelector('.nav__list').classList.remove('transformation');
+        scrollTop();
+    }
+
+    if (e.target.classList.contains('nav__link') && !e.target.classList.contains('nav__link--drop')) {
+        e.preventDefault();
+        closeMenu();
+    }
+});
+
+document.addEventListener('click', (e) => {
+    if (!menu.contains(e.target) && !burger.contains(e.target) && !overlay.contains(e.target)) {
+        closeMenu();
+    }
+});
+
+
+
+});
+
+
